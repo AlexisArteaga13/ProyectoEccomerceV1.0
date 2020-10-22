0f0aac1f9495586612c5bf9f8a2a308ad37acccd
AlexisArteaga13 <alexisarteagacarhua@gmail.com>
2020-10-22 17:48:18 -0500

    botonescorregidosmenosvisibility

--- emprendevende_back/resources/views/vistasadmin/indexadmin.blade.php
+++ emprendevende_back/resources/views/vistasadmin/indexadmin.blade.php
@@ -187,14 +187,15 @@
 
 <!-- jQuery -->
 <script src="{{asset('adminlte/plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <!-- jQuery UI 1.11.4 -->
 <script src="{{asset('adminlte/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button)
 </script>
-<!-- Bootstrap 4 -->
-<script src="{{asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
 <!-- ChartJS -->
 <script src="{{asset('adminlte/plugins/chart.js/Chart.min.js')}}"></script>
 <!-- Sparkline -->
--- emprendevende_back/resources/views/vistasadmin/rubros/rindex.blade.php
+++ emprendevende_back/resources/views/vistasadmin/rubros/rindex.blade.php
@@ -8,12 +8,12 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1>DataTables</h1>
+                        <h1>Rubros</h1>
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">DataTables</li>
+                            <li class="breadcrumb-item active">Rubros</li>
                         </ol>
                     </div>
                 </div>
@@ -30,7 +30,7 @@
 
                         <div class="card">
                             <div class="card-header">
-                                <h3 class="card-title">DataTable with default features</h3>
+                                <h3 class="card-title">Rubros</h3>
                             </div>
                             <!-- /.card-header -->
                             <div class="card-body">
