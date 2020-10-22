4f9d0310ece6b6eed8e07f701fdcc8b83e152bae
AlexisArteaga13 <alexisarteagacarhua@gmail.com>
2020-10-22 17:52:06 -0500

    Rubros

--- emprendevende_back/resources/views/vistasadmin/indexadmin.blade.php
+++ emprendevende_back/resources/views/vistasadmin/indexadmin.blade.php
@@ -237,16 +237,16 @@
 <script>
   $(function () {
     $("#example1").DataTable({
-      "responsive": true, "lengthChange": false, "autoWidth": false,
+      "responsive": true, "lengthChange": true, "autoWidth": true,
       "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
     }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
     $('#example2').DataTable({
       "paging": true,
-      "lengthChange": false,
-      "searching": false,
+      "lengthChange": true,
+      "searching": true,
       "ordering": true,
       "info": true,
-      "autoWidth": false,
+      "autoWidth": true,
       "responsive": true,
     });
   });
